f940b45b988df47706c518e14589b412b6539b9f
Даниил Луценко <110895420+DefaultDari@users.noreply.github.com>
2024-10-03 10:25:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 month = int(input("Введите месяц (от 1 до 12) : ")) #Ввод месяца
 day = int(input("Введите число: ")) #Ввод дня
 # Определеям время года с if/elif/else:
-if (month == 3 and day >= 1 and day>=31) or (month == 4 and day<=1 and day<=30) or (month == 5 and day>=1 and day <=31): #Проверка на весну
+if (month == 3 and day >= 1 and day<=31) or (month == 4 and day>=1 and day<=30) or (month == 5 and day>=1 and day <=31): #Проверка на весну
     time = "Весна"
     print(f"Время года : {time}") #Выводим результат на экран
-elif (month == 6 and day >= 1 and day <= 30) or (month == 7 and day<=1 and day<=31) or (month == 8 and day >= 1 and day <= 31): #Проверка на лето
+elif (month == 6 and day >= 1 and day <= 30) or (month == 7 and day>=1 and day<=31) or (month == 8 and day >= 1 and day <= 31): #Проверка на лето
     time = "Лето" 
     print(f"Время года : {time}") #Выводим результат на экран
-elif (month == 9 and day >= 1 and day <= 30) or (month == 10 and day<=1 and day<=31) or (month == 11 and day >= 1 and day <=30): #Проверка на осень
+elif (month == 9 and day >= 1 and day <= 30) or (month == 10 and day>=1 and day<=31) or (month == 11 and day >= 1 and day <=30): #Проверка на осень
     time = "Осень"
     print(f"Время года : {time}") #Выводим результат на экран
 elif (month == 12  and day >= 1 and day <= 31) or (month == 1 and day >= 1 and day < 31) or (month == 2 and day >= 1 and day <= 28): #Проверка на зиму
